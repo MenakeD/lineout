48a0a0214d88ab60489233c50028e537b8b472b4
SankhajaH <shapu953@gmial.com>
2022-09-02 22:19:30 +0530

    add: ui customer dashboard

--- pages/customer/dashboard.tsx
+++ pages/customer/dashboard.tsx
@@ -5,14 +5,18 @@ import { AiOutlineQrcode } from 'react-icons/ai'
 import { VscGraphLine } from 'react-icons/vsc'
 import { CgNotes } from 'react-icons/cg'
 import { AiOutlineQuestionCircle } from 'react-icons/ai'
+import QueueData from '../../components/common/QueueData'
 
 const Dashboard = () => {
   const size = 100
   const color = 'white'
   return (
     <Container>
-      <p>Dashboard</p>
-      <div className='grid grid-cols-2 gap-4'>
+      <h1 className='font-bold text-xl text-white'>Your Queues</h1>
+      <div className='py-2'>
+        <QueueData />
+      </div>
+      <div className='grid grid-cols-2 gap-2 pt-4'>
         <OptionCard
           Icon={<AiOutlineQrcode size={size} color={color} />}
           Text='View QR code'
